841c8d84cfd76162ec3b96b6062cf25ccbd6474b
Jenslee Dsouza <dsouzajenslee@gmail.com>
2025-10-02 13:30:56 +0530

    tests

---
+++ packages/valory/skills/funds_manager/tests/data_for_tests.py
@@ -0,0 +1,234 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2025 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains test data for funds manager skill."""
+
+
+MOCK_AGENT_ADDRESS = "0x1111111111111111111111111111111111111111"
+MOCK_SAFE_ADDRESS = "0x2222222222222222222222222222222222222222"
+
+
+MOCK_RPC_URLS = {
+    "optimism": "https://mock-rpc.com/optimism",
+    "base": "https://mock-rpc.com/base",
+    "mode": "https://mock-rpc.com/mode",
+    "gnosis": "https://mock-rpc.com/gnosis",
+}
+
+OPTIMUS_INITIAL_FUND_REQUIREMENTS = {
+    "optimism": {
+        "agent": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 5000000000000000,
+                "threshold": 2500000000000000,
+            },
+            "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85": {"topup": 0, "threshold": 0},
+        },
+        "safe": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 15000000000000000,
+                "threshold": 7500000000000000,
+            },
+            "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85": {
+                "topup": 40000000,
+                "threshold": 20000000,
+            },
+        },
+    },
+    "base": {
+        "agent": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 5000000000000000,
+                "threshold": 2500000000000000,
+            },
+            "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": {"topup": 0, "threshold": 0},
+        },
+        "safe": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 15000000000000000,
+                "threshold": 7500000000000000,
+            },
+            "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": {
+                "topup": 40000000,
+                "threshold": 20000000,
+            },
+        },
+    },
+    "mode": {
+        "agent": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 5000000000000000,
+                "threshold": 2500000000000000,
+            },
+            "0xd988097fb8612cc24eeC14542bC03424c656005f": {"topup": 0, "threshold": 0},
+        },
+        "safe": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 15000000000000000,
+                "threshold": 7500000000000000,
+            },
+            "0xd988097fb8612cc24eeC14542bC03424c656005f": {
+                "topup": 40000000,
+                "threshold": 20000000,
+            },
+        },
+    },
+}
+
+OPTIMUS_MULTICALL_RETURN_VALUES = [
+    [0, 2500000000000000],  # eth, usdc agent balance on optimism
+    [6],  # usdc decimals
+    [0, 0],  # eth, usdc safe balance on optimism
+    [6],  # usdc decimals
+    [0, 0],  # eth, usdc agent balance on base
+    [6],  # usdc decimals
+    [2500000000000000, 0],  # eth, usdc safe balance on base
+    [6],  # usdc decimals
+    [0, 0],  # eth, usdc agent balance on mode
+    [6],  # usdc decimals
+    [0, 0],  # eth, usdc safe balance on mode
+    [6],  # usdc decimals
+]
+
+
+OPTIMUS_FUNDS_RESPONSE = {
+    "optimism": {
+        "0x1111111111111111111111111111111111111111": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "0",
+                "deficit": "5000000000000000",
+                "decimals": 18,
+            },
+            "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85": {
+                "balance": "2500000000000000",
+                "deficit": "0",
+                "decimals": 6,
+            },
+        },
+        "0x2222222222222222222222222222222222222222": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "0",
+                "deficit": "15000000000000000",
+                "decimals": 18,
+            },
+            "0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85": {
+                "balance": "0",
+                "deficit": "40000000",
+                "decimals": 6,
+            },
+        },
+    },
+    "base": {
+        "0x1111111111111111111111111111111111111111": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "0",
+                "deficit": "5000000000000000",
+                "decimals": 18,
+            },
+            "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": {
+                "balance": "0",
+                "deficit": "0",
+                "decimals": 6,
+            },
+        },
+        "0x2222222222222222222222222222222222222222": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "2500000000000000",
+                "deficit": "12500000000000000",
+                "decimals": 18,
+            },
+            "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913": {
+                "balance": "0",
+                "deficit": "40000000",
+                "decimals": 6,
+            },
+        },
+    },
+    "mode": {
+        "0x1111111111111111111111111111111111111111": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "0",
+                "deficit": "5000000000000000",
+                "decimals": 18,
+            },
+            "0xd988097fb8612cc24eeC14542bC03424c656005f": {
+                "balance": "0",
+                "deficit": "0",
+                "decimals": 6,
+            },
+        },
+        "0x2222222222222222222222222222222222222222": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "0",
+                "deficit": "15000000000000000",
+                "decimals": 18,
+            },
+            "0xd988097fb8612cc24eeC14542bC03424c656005f": {
+                "balance": "0",
+                "deficit": "40000000",
+                "decimals": 6,
+            },
+        },
+    },
+}
+
+
+TRADER_INITIAL_FUND_REQUIREMENTS = {
+    "gnosis": {
+        "agent": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 100000000000000000,
+                "threshold": 50000000000000000,
+            }
+        },
+        "safe": {
+            "0x0000000000000000000000000000000000000000": {
+                "topup": 5000000000000000000,
+                "threshold": 2500000000000000000,
+            }
+        },
+    }
+}
+
+TRADER_MULTICALL_RETURN_VALUES = [
+    [50000000000000000 - 1],  # agent balance on gnosis
+    [18],  # xdai decimals
+    [2500000000000000000],  # safe balance on gnosis
+    [18],  # xdai decimals
+]
+
+
+TRADER_FUNDS_RESPONSE = {
+    "gnosis": {
+        "0x1111111111111111111111111111111111111111": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "49999999999999999",
+                "deficit": "50000000000000001",
+                "decimals": 18,
+            }
+        },
+        "0x2222222222222222222222222222222222222222": {
+            "0x0000000000000000000000000000000000000000": {
+                "balance": "2500000000000000000",
+                "deficit": "0",
+                "decimals": 18,
+            }
+        },
+    }
+}
---
+++ packages/valory/skills/funds_manager/tests/test_funds_manager_behaviour.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2025 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the tests for valory/decision_maker_abci's base behaviour."""
+
+from pathlib import Path
+from typing import Any, Dict, List, cast
+from unittest import mock
+from unittest.mock import MagicMock
+
+import pytest
+from aea.configurations.base import PackageConfiguration
+from aea.test_tools.test_skill import BaseSkillTestCase
+
+from packages.valory.skills.funds_manager.behaviours import (
+    FundsManagerBehaviour,
+    GET_FUNDS_STATUS_METHOD_NAME,
+)
+from packages.valory.skills.funds_manager.models import FundRequirements
+from packages.valory.skills.funds_manager.tests.data_for_tests import (
+    MOCK_AGENT_ADDRESS,
+    MOCK_RPC_URLS,
+    MOCK_SAFE_ADDRESS,
+    OPTIMUS_FUNDS_RESPONSE,
+    OPTIMUS_INITIAL_FUND_REQUIREMENTS,
+    OPTIMUS_MULTICALL_RETURN_VALUES,
+    TRADER_FUNDS_RESPONSE,
+    TRADER_INITIAL_FUND_REQUIREMENTS,
+    TRADER_MULTICALL_RETURN_VALUES,
+)
+
+
+CURRENT_FILE_PATH = Path(__file__).resolve()
+PACKAGE_DIR = CURRENT_FILE_PATH.parents[1]
+
+
+class TestFundsManagerBehaviour(BaseSkillTestCase):
+    """Test `TestFundsManagerBehaviour`."""
+
+    # behaviour: FundsManager  # type: ignore
+    path_to_skill = PACKAGE_DIR
+    _skill = MagicMock()
+
+    @property
+    def behaviour(self) -> FundsManagerBehaviour:
+        """Get the behaviour."""
+        return cast(FundsManagerBehaviour, self.skill.behaviours["funds_manager"])
+
+    @classmethod
+    def setup_class(cls, **kwargs: Any) -> None:
+        """Set up the class."""
+        kwargs["config_overrides"] = {
+            "models": {
+                "params": {
+                    "args": {
+                        "fund_requirements": TRADER_INITIAL_FUND_REQUIREMENTS,
+                        "safe_address": MOCK_SAFE_ADDRESS,
+                        "rpc_urls": MOCK_RPC_URLS,
+                    },
+                }
+            }
+        }
+
+        with mock.patch.object(PackageConfiguration, "check_overrides_valid"):
+            super().setup_class(**kwargs)
+        super().setup_class(**kwargs)
+
+    def setup(self, **kwargs: Any) -> None:
+        """Setup."""
+        self.behaviour.setup()
+        super().setup(**kwargs)
+
+    def test_safe_address(self) -> None:
+        """Test the `safe_address` property."""
+        assert self.behaviour.safe_address == MOCK_SAFE_ADDRESS
+
+    def test_get_funds_status_exists_in_shared_state(self) -> None:
+        """Test the `get_funds_status` method is correctly set in the shared state."""
+
+        assert GET_FUNDS_STATUS_METHOD_NAME in self.behaviour.context.shared_state
+        assert callable(
+            self.behaviour.context.shared_state[GET_FUNDS_STATUS_METHOD_NAME]
+        )
+
+    @pytest.mark.parametrize(
+        "fund_requirements, funds_response, mock_multicall_response",
+        [
+            (
+                TRADER_INITIAL_FUND_REQUIREMENTS,
+                TRADER_FUNDS_RESPONSE,
+                TRADER_MULTICALL_RETURN_VALUES,
+            ),
+            (
+                OPTIMUS_INITIAL_FUND_REQUIREMENTS,
+                OPTIMUS_FUNDS_RESPONSE,
+                OPTIMUS_MULTICALL_RETURN_VALUES,
+            ),
+        ],
+        ids=["trader", "optimus"],
+    )
+    def test_get_funds_status(
+        self,
+        fund_requirements: Dict[str, Any],
+        funds_response: Dict[str, Any],
+        mock_multicall_response: List[List[Any]],
+    ) -> None:
+        """Test the `get_funds_status` method."""
+        behaviour = self.behaviour
+        behaviour.context.params.fund_requirements = FundRequirements.from_dict(
+            fund_requirements
+        )
+
+        # patch the instance method
+        behaviour.perform_w3_multicall = mock.Mock(side_effect=mock_multicall_response)  # type: ignore
+
+        with mock.patch.object(
+            type(behaviour.context),
+            "agent_address",
+            new_callable=mock.PropertyMock,
+            return_value=MOCK_AGENT_ADDRESS,
+        ):
+
+            funds = cast(FundRequirements, behaviour.get_funds_status())
+
+        assert behaviour.perform_w3_multicall.call_count == len(mock_multicall_response)
+
+        assert funds.get_response_body() == funds_response
